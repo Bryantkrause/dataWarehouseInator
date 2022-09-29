5cfd57700049f667f2d99cb16173d70f57725b11
bryantkrause <bryantkrause@gmail.com>
2022-09-29 10:54:22 -0700

    converted intial approvers and location data

--- seed.py
+++ seed.py
@@ -37,7 +37,7 @@ df = pd.read_csv(file, usecols=columns, parse_dates=["Date"])
 
 df.rename(columns={
     'Voucher #': 'Voucher',
-    'Ref #':'PurchaseOrder',
+    'Ref #': 'PurchaseOrder',
     'Item ID': 'ItemID',
     'U/M': 'Unit',
     'Total base': 'TotalBase',
@@ -49,10 +49,10 @@ df['Unit'].fillna("Blank", inplace=True)
 df['Taxes'].fillna("0.0", inplace=True)
 df['Memo'].fillna("0.0", inplace=True)
 df['Comments'].fillna("0.0", inplace=True)
-df['Approvers'].fillna("0.0", inplace=True)
 df['PurchaseOrder'].fillna("0.0", inplace=True)
 df['PrimaryKey'] = df['Voucher'] + df['Vendor'] + df['Line'].astype(str)
-
+df['Approvers'].fillna("No", inplace=True)
+print(df['Approvers'].unique())
 
 Item = df['ItemID'].unique()
 Account = df['Account'].unique()
@@ -63,21 +63,37 @@ df['Approvers'] = df['Approvers'].map(
     lambda x: x.rstrip('pending').rstrip('reject'))
 
 Approvers = df['Approvers'].unique()
-
-ALoc = pd.DataFrame(Approvers)
-ALoc.columns
-mask = ALoc['0'].str.len() < 6
-ALoc2 = ALoc.loc[mask]
-
+print(Approvers)
+ALoc2 = pd.DataFrame(Approvers, columns=['Name'])
+# for col in ALoc.columns:
+#     print(col)
+# ALoc.rename(columns={0: 'Name'},inplace=True)
+# for col in ALoc.columns:
+#     print(col)
+# ALoc['length'] = ALoc['Name'].str.len()
+# print(ALoc)
+# print(ALoc.sort_values(by=['Name']))
+# mask = ALoc['length'] > 6
+# ALoc2 = ALoc.loc[mask]
+# ALoc2['Name'].fillna("0", inplace=True)
 print(ALoc2)
-appCondition = ['Cerritos', 'Rancho', 'Fullerton',
-                'Downey', 'Rancho', 'Rancho', 'Rancho']
-appChoice = ['FrankGuzma' ,'CarlosGarcia', 'LuisSerrano', 'HenryValdez', 'DerekFranco',
-              'DavidRevolorio', 'RobertOrtega']
-# Approvers['Location'] = np.select(appCondition, appChoice, default='NA')
+appChoice = ['Cerritos', 'Rancho', 'Fullerton',
+             'Downey', 'Rancho', 'Rancho', 'Rancho','None']
+appCondition = [
+    ALoc2['Name'].str.contains(r'FrankGuzma', na=False),
+    ALoc2['Name'].str.contains(r'CarlosGarcia', na=False),
+    ALoc2['Name'].str.contains(r'LuisSerrano', na=False),
+    ALoc2['Name'].str.contains(r'HenryValdez', na=False),
+    ALoc2['Name'].str.contains(r'DerekFranco', na=False),
+    ALoc2['Name'].str.contains(r'DavidRevolorio', na=False),
+    ALoc2['Name'].str.contains(r'RobertOrtega', na=False),
+    ALoc2['Name'].str.contains(r'No', na=False)]
 
+print(appCondition)
+print(appChoice)
+ALoc2['Location'] = np.select(appCondition, appChoice, default='NA')
+print(ALoc2)
 # print(Item)
 # database_connection = create_engine(f'mysql://root:{pw}@localhost/{db}')
 # df.to_sql(con=database_connection,
 #           name='rawdata', if_exists='replace')
-
