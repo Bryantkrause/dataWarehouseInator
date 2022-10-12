180f2a176d412f811c66c93c9329a93321b81e23
bryantkrause <bryantkrause@gmail.com>
2022-10-12 14:28:18 -0700

    update categories

--- Revenueinator.py
+++ Revenueinator.py
@@ -15,7 +15,71 @@ columnNames = [
 df.columns = columnNames
 df['Category'] = df['Category'].fillna(method='ffill')
 df = df.dropna(subset=['Date'])
-# df = df[df['Category'].str.contains("Total")==False]
+
+
+dropme = str('01.21 (Warehousing services for the month of January 2021)')
+
+df = df[df['Category'].str.contains(dropme) == False]
+
+conditions = [
+    df['Category'].str.contains(
+        r'Bank Fee (ACH/Wire Bank Fee)', na=False),
+    df['Category'].str.contains(
+        r'CH (Administrative Support)', na=False),
+    df['Category'].str.contains(
+        r'EDI (EDI Fees)', na=False),
+    df['Category'].str.contains(
+        r'Rescheduing Fee (Rescheduling fee)', na=False),
+    df['Category'].str.contains(
+        r'Rescheduing Fee (Rescheduling fee)', na=False),
+    df['Category'].str.contains(
+        r'Rescheduling fee (Rescheduling fee)', na=False),
+    df['Category'].str.contains(
+        r'Delv Service (Transportation Services)', na=False),
+    df['Category'].str.contains(
+        r'Dray La Mirada (Drayage Container:)', na=False),
+    df['Category'].str.contains(
+        r'Inbound Handling - PH (Inbound Handling)', na=False),
+    df['Category'].str.contains(
+        r'OF Inbound Handling - PH (Inbound Handling: Overflow)', na=False),
+    df['Category'].str.contains(
+        r'MTRLS (Materials Supplied)', na=False),
+    df['Category'].str.contains(
+        r'OF Outbound Handling - PH (Outbound Handling:', na=False),
+    df['Category'].str.contains(
+        r'PH (Product Handling Services) - Other', na=False),
+    df['Category'].str.contains(
+        r'Fedex (Fedex Services)', na=False),
+    df['Category'].str.contains(
+        r'Postage', na=False),
+    df['Category'].str.contains(
+        r'Rework (Rework)', na=False),
+    df['Category'].str.contains(
+        r'MO MIN (Minimum Monthly Service Charge)', na=False),
+    df['Category'].str.contains(
+        r'OF PS (Re-Occurring Storage: Overflow)', na=False),
+    df['Category'].str.contains(
+        r'PS (Re-Occurring Storage) - Other', na=False),
+    df['Category'].str.contains(
+        r'PS (Re-Occurring Storage) - Other', na=False),
+
+]
+
+choices = [
+    'Clerical', 'Clerical',
+    'Clerical', 'Clerical',
+    'Clerical', 'Drayage',
+    'Drayage', 'Inbound',
+    'Inbound', 'Materials',
+    'Outbound', 'Outbound',
+    'Outbound', 'Postage',
+    'Postage', 'Rework',
+    'Storage', 'Storage',
+    'Storage', 'Storage',
+    'Storage'
+]
+
+df['CategoryType'] = np.select(conditions, choices, default='None')
 # df = df[~df.Category.str.contains("Total")]
 # df = df[df['Category'] != 'Total']
 
