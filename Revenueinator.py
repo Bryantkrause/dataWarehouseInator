50dd3b41a29d669055bdcade45918a5c2b2b5b9c
bryantkrause <bryantkrause@gmail.com>
2022-10-12 10:41:01 -0700

    much cleaner

--- Revenueinator.py
+++ Revenueinator.py
@@ -4,19 +4,24 @@ from sqlalchemy import column
 
 
 useColumns = [
-    'Unnamed: 0','Date', 'Num', 'Memo', 'Name', 'Amount'
+    'Unnamed: 0', 'Date', 'Num', 'Memo', 'Name', 'Amount'
 ]
 file = r'C:\Users\bkrause\Documents\RevenueRaw1012222.CSV'
-
+updatedFile = r'C:\Users\bkrause\Documents\refinedRev.xlsx'
 df = pd.read_csv(file, usecols=useColumns, parse_dates=["Date"])
 columnNames = [
-    'Category','Date','InvoiceNumber', 'Memo', 'Name', 'Amount'
+    'Category', 'Date', 'InvoiceNumber', 'Memo', 'Name', 'Amount'
 ]
 df.columns = columnNames
-df = df.fillna(method='ffill')
+df['Category'] = df['Category'].fillna(method='ffill')
+df = df.dropna(subset=['Date'])
+# df = df[df['Category'].str.contains("Total")==False]
+# df = df[~df.Category.str.contains("Total")]
+# df = df[df['Category'] != 'Total']
 
 print(df.head())
 print(df.info())
 
-
-
+writer = pd.ExcelWriter(updatedFile, engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Revenue')
+writer.close()
