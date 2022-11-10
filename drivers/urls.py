20ad40694d9e00a7c4f721a30b37631033106959
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-11-10 06:49:28 -0800

    ok so probably in my best interest to refactor this second app into its own

--- drivers/urls.py
+++ drivers/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.driver_index, name="driver_index"),
+    path("<contractor>", views.driver_index, name="contractor_index"),
     path("<int:pk>/", views.driver_detail, name="driver_detail"),
-    path("<contractor>/", views.driver_detail, name="contractor_detail"),
+    path("<int:pk>/", views.contractor_detail, name="contractor_detail"),
 ]
