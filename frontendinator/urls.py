e98e7c714d0c4fc9737324900b25548eec120932
bryantkrause <bryantkrause@gmail.com>
2022-11-11 13:44:39 -0800

    get un needed thingy outa here

--- frontendinator/urls.py
+++ frontendinator/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contractors/', include("contractors.urls")),
-    path('driver/', include("contractors.urls")),
 ]
