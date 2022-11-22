74813fc83b0bfc461a317cd1b36c2e88971e6416
bryantkrause@gmail.com <bryantkrause@gmail.com>
2022-11-22 05:46:20 -0800

    digging up the corpses

--- drivers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.driver_index, name="driver_index"),
-    path("<int:pk>/", views.driver_detail, name="driver_detail"),
-
-    path("accounts/", include("django.contrib.auth.urls")),
-]
