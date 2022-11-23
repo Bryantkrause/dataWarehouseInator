18dbb398210512422a7c702931ee4fe5799006e1
bryantkrause <bryantkrause@gmail.com>
2022-11-23 13:04:50 -0800

    git er dun

--- laborinatorconvertinator.py
+++ laborinatorconvertinator.py
@@ -39,7 +39,8 @@ columnNames = [
 df.columns = columnNames
 # remove bogus rows
 df = df[1:-1]
-
+df['EndPeriod'] = pd.to_datetime(df['EndPeriod']).dt.normalize()
+# clean hour data
 # Set columns to create total from
 hourTotal = ['RegularHours', 'OvertimeHours',
              'DoubleTimeHours', 'HolidayHours', 'SickHours']
@@ -47,8 +48,8 @@ hourTotal = ['RegularHours', 'OvertimeHours',
 depHours = df.groupby(['DivisionCode', 'DepartmentCode', 'EndPeriod'])[
     'RegularHours', 'OvertimeHours', 'DoubleTimeHours', 'HolidayHours', 'SickHours'].sum().reset_index()
 # update object data type to date type
-depHours['EndPeriod'] = pd.to_datetime(
-    depHours['EndPeriod'])
+# depHours['EndPeriod'] = pd.to_datetime(
+#     depHours['EndPeriod'])
 # add total column
 depHours['TotalHours'] = depHours[hourTotal].sum(axis=1)
 # add Ot percantage column
@@ -63,13 +64,13 @@ depHours['DepartmentCode'] = depHours['DepartmentCode'].astype(np.int64)
 depHours['DivisionCode'] = depHours['DivisionCode'].astype(np.int64)
 # remove managerial data
 depHours = depHours.drop(
-    depHours[depHours.DepartmentCode.isin([104, 107, 204, 207])].index)
+    depHours[depHours.DepartmentCode.isin([104, 107, 204, 207, 114, 212])].index)
 # update incorrect information of 20 to 9278 location
 depHours.DivisionCode.replace(20, 9278, inplace=True)
 # convert Downey CSR to Rancho CSR
 depHours.loc[depHours.DepartmentCode == 109, 'DivisionCode'] = 9278
 
-
+# clean pay data
 # Set columns to create total from
 payTotal = ['RegularPay', 'OvertimePay',
             'DoubleTimePay', 'HolidayPay', 'SickPay']
@@ -77,8 +78,8 @@ payTotal = ['RegularPay', 'OvertimePay',
 depPay = df.groupby(['DivisionCode', 'DepartmentCode', 'EndPeriod'])[
     'RegularPay', 'OvertimePay', 'DoubleTimePay', 'HolidayPay', 'SickPay'].sum().reset_index()
 # update object data type to date
-depPay['EndPeriod'] = pd.to_datetime(
-    depPay['EndPeriod'])
+# depPay['EndPeriod'] = pd.to_datetime(depPay['EndPeriod'])
+# depPay['EndPeriod'] = pd.to_datetime(depPay['EndPeriod'])
 # add total column
 depPay['TotalPay'] = depPay[payTotal].sum(axis=1)
 # add Ot percantage column
@@ -93,16 +94,30 @@ depPay['DepartmentCode'] = depPay['DepartmentCode'].astype(np.int64)
 depPay['DivisionCode'] = depPay['DivisionCode'].astype(np.int64)
 # remove managerial data
 depPay = depPay.drop(
-    depPay[depPay.DepartmentCode.isin([104, 107, 204, 207])].index)
+    depPay[depPay.DepartmentCode.isin([104, 107, 204, 207, 114, 212])].index)
 # update incorrect information of 20 to 9278 location
 depPay.DivisionCode.replace(20, 9278, inplace=True)
 # convert Downey CSR to Rancho CSR
 depPay.loc[depPay.DepartmentCode == 109, 'DivisionCode'] = 9278
 
+# set up location based dataframes
+
+# fullerton data
+fullertonDataPay = depPay.loc[depPay['DivisionCode'] == 210]
+getUniqueDeps = fullertonDataPay.DepartmentCode.value_counts()
+getRowCount = getUniqueDeps.count()
+
+fPivotPayReg = fullertonDataPay.pivot_table(
+    columns='DepartmentCode', index='EndPeriod', aggfunc='sum', values='RegularPay')
+fPivotPayOT = fullertonDataPay.pivot_table(columns=pd.Grouper(
+    freq='W', key='EndPeriod'), index='DepartmentCode', aggfunc='sum', values='OvertimePay')
+
+
 # set up excel file to save and save data to specific tabs
 writer = pd.ExcelWriter(updatedFile, engine='xlsxwriter')
 depHours.to_excel(writer, sheet_name='Hours')
 depPay.to_excel(writer, sheet_name='Pay')
+fPivotPayReg.to_excel(writer, sheet_name='Fullerton')
 writer.close()
 
 # use this line to turn into percentages:
